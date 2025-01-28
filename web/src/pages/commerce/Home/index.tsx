5020960a3bd6eb2dd6a2ceb3f87a18a59d42ef74
Lucas Camargo <lfqcamargo@gmail.com>
2025-01-27 21:45:52 -0300

    Implementado carrosel

--- web/src/pages/commerce/Home/index.tsx
+++ web/src/pages/commerce/Home/index.tsx
@@ -1,13 +1,18 @@
+import { useState } from 'react'
 import { CardProduct } from '../../../components/CardProduct'
 import {
+  ArrowButton,
+  CarouselContent,
+  CarouselWrapper,
   Container,
   ContainerBanner,
+  ContainerCarousel,
   ContainerLastChange,
   ContainerMain,
-  ContainerProducts,
   ContainersectionProducts,
+  ContainerViewAll,
 } from './styles'
-import { FaAngleRight } from 'react-icons/fa'
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa'
 
 const productsLastChanges = [
   {
@@ -65,43 +70,138 @@ const productsLastChanges = [
     'stars': 5,
     'starsQuantity': 538,
   },
+  {
+    'id': '6',
+    'img': '/product-apagar.png',
+    'title': 'Console PlayStation 5 Slim Sony, SSD 1TB, Edição Digital, Com Controle Sem Fio ,…',
+    'price': 3999.90,
+    'offerPrice': 3700.90,
+    'discountPix': 5,
+    'offerTime': '28/01/2025',
+    'stars': 5,
+    'starsQuantity': 538,
+  },
+  {
+    'id': '7',
+    'img': '/product-apagar.png',
+    'title': 'Console PlayStation 5 Slim Sony, SSD 1TB, Edição Digital, Com Controle Sem Fio ,…',
+    'price': 3999.90,
+    'offerPrice': 3700.90,
+    'discountPix': 5,
+    'offerTime': '28/01/2025',
+    'stars': 5,
+    'starsQuantity': 538,
+  },
+  {
+    'id': '8',
+    'img': '/product-apagar.png',
+    'title': 'Console PlayStation 5 Slim Sony, SSD 1TB, Edição Digital, Com Controle Sem Fio ,…',
+    'price': 3999.90,
+    'offerPrice': 3700.90,
+    'discountPix': 5,
+    'offerTime': '28/01/2025',
+    'stars': 5,
+    'starsQuantity': 538,
+  },
+  {
+    'id': '9',
+    'img': '/product-apagar.png',
+    'title': 'Console PlayStation 5 Slim Sony, SSD 1TB, Edição Digital, Com Controle Sem Fio ,…',
+    'price': 3999.90,
+    'offerPrice': 3700.90,
+    'discountPix': 5,
+    'offerTime': '28/01/2025',
+    'stars': 5,
+    'starsQuantity': 538,
+  },
+  {
+    'id': '10',
+    'img': '/product-apagar.png',
+    'title': 'Console PlayStation 5 Slim Sony, SSD 1TB, Edição Digital, Com Controle Sem Fio ,…',
+    'price': 3999.90,
+    'offerPrice': 3700.90,
+    'discountPix': 5,
+    'offerTime': '28/01/2025',
+    'stars': 5,
+    'starsQuantity': 538,
+  },
+  {
+    'id': '11',
+    'img': '/product-apagar.png',
+    'title': 'Console PlayStation 5 Slim Sony, SSD 1TB, Edição Digital, Com Controle Sem Fio ,…',
+    'price': 3999.90,
+    'offerPrice': 3700.90,
+    'discountPix': 5,
+    'offerTime': '28/01/2025',
+    'stars': 5,
+    'starsQuantity': 538,
+  },
 
 ]
 
 export function Home() {
-  return(
-    <>
-      <Container>
-        <ContainerMain>
-          <ContainerBanner>
-            <img src="banner-apagar.png" alt="Banner" />
-          </ContainerBanner>
-          <ContainerLastChange>
-            <p>ÚLTIMA CHANCE</p>
-          </ContainerLastChange>
-          <ContainersectionProducts>
-            <ContainerProducts>
-              {productsLastChanges.map((product) => (
-                <CardProduct
-                  id={product.id}
-                  img={product.img}
-                  title={product.title}
-                  price={product.price}
-                  offerPrice={product.offerPrice}
-                  discountPix={product.discountPix}
-                  offerTime={product.offerTime}
-                  stars={product.stars}
-                  starsQuantity={product.starsQuantity}
-                />
-              ))}
-            </ContainerProducts>
-            <div>
-              <p>VER TODOS <FaAngleRight size={14}/></p>
-            </div>
-          </ContainersectionProducts>
+  const [currentIndex, setCurrentIndex] = useState(0)
 
-        </ContainerMain>
-      </Container>
-    </>
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === productsLastChanges.length - 4 ? 0 : prevIndex + 1,
+    )
+  }
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? productsLastChanges.length - 4 : prevIndex - 1,
+    )
+  }
+
+  return (
+    <Container>
+      <ContainerMain>
+        <ContainerBanner>
+          <img src="banner-apagar.png" alt="Banner" />
+        </ContainerBanner>
+        <ContainerLastChange>
+          <p>ÚLTIMA CHANCE</p>
+        </ContainerLastChange>
+        <ContainersectionProducts>
+          <ContainerCarousel>
+            <ArrowButton onClick={handlePrev}>
+              <FaAngleLeft />
+            </ArrowButton>
+            <CarouselWrapper>
+
+              <CarouselContent
+                itemsCount={productsLastChanges.length}
+                style={{
+                  transform: `translateX(-${currentIndex * 25}%)`,
+                }}
+              >
+                {productsLastChanges.map((product) => (
+                  <CardProduct
+                    id={product.id}
+                    img={product.img}
+                    title={product.title}
+                    price={product.price}
+                    offerPrice={product.offerPrice}
+                    discountPix={product.discountPix}
+                    offerTime={product.offerTime}
+                    stars={product.stars}
+                    starsQuantity={product.starsQuantity}
+                  />
+                ))}
+              </CarouselContent>
+            </CarouselWrapper>
+            <ArrowButton onClick={handleNext}>
+              <FaAngleRight />
+            </ArrowButton>
+          </ContainerCarousel>
+
+          <ContainerViewAll>
+            <p>VER TODOS</p>
+            <FaAngleRight size={14} />
+          </ContainerViewAll>
+        </ContainersectionProducts>
+      </ContainerMain>
+    </Container>
   )
 }
--- web/src/pages/commerce/Home/styles.ts
+++ web/src/pages/commerce/Home/styles.ts
@@ -39,11 +39,64 @@ export const ContainersectionProducts = styled.section`
   align-items: center;
 `
 
-export const ContainerProducts = styled.div`
+export const ContainerCarousel = styled.div`
   display: flex;
-  width: 1408px;
-  flex-direction: row;
-  margin: auto;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 46px 32px 22px 32px;
+`
+
+export const CarouselWrapper = styled.div`  
+  display: flex;
+  align-items: center;
+
+  overflow: hidden;   
+`
+
+interface CarouselContentProps {
+  itemsCount: number;
+}
+export const CarouselContent = styled.div<CarouselContentProps>`
+  display: flex;
+  width: 100%;
 
   gap: 8px;
+  transition: transform 0.5s ease-in-out;
+`
+
+export const ArrowButton = styled.button`
+  background-color: ${(props) => props.theme['white-bg']};
+  border: none;
+  color: ${(props) => props.theme['gray-100']};
+  font-size: 24px;
+  height: 32px;
+  padding: 10px;
+  cursor: pointer;
+  z-index: 10;
+
+  &:nth-child(1) {
+    left: 10px;
+  }
+
+  &:nth-child(3) {
+    right: 10px;
+  }
+`
+
+export const ContainerViewAll = styled.div`
+  display: flex;
+  width: 1408px;
+
+  align-items: center;
+  justify-content: end; 
+  gap: 4px;
+
+  color: ${(props) => props.theme['secondary-500']};
+  
+  p {    
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 24px;
+  }
 `
